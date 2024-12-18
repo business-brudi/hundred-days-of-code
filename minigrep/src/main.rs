5b07b630f51703b90d4f82103eab925670a46992
Codebrudi <codebrudi@pm.me>
2024-12-18 11:32:52 +0100

    [day-013] refatoring: use struct for configuration

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,19 +4,24 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_configuration(&args);
-    println!("searching for '{query}'");
-    println!("in file '{file_path}'");
+    let configuration = parse_configuration(&args);
+    println!("searching for '{}'", configuration.query);
+    println!("in file '{}'", configuration.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(configuration.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
 
-fn parse_configuration(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Configuration {
+    query: String,
+    file_path: String
+}
 
-    (query, file_path)
+fn parse_configuration(args: &[String]) -> Configuration {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Configuration { query, file_path }
 }
