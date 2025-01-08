b5c78819bc5304607169146c5e24c7365a0f7351
Codebrudi <34597495+business-brudi@users.noreply.github.com>
2025-01-08 20:59:38 +0100

    added meta board data

--- tower_wars_clone/Assets/Scripts/BoardManager.cs
+++ tower_wars_clone/Assets/Scripts/BoardManager.cs
@@ -3,6 +3,12 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData
+    {
+        public bool Passable;
+    }
+
+    private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
 
     public int Width;
@@ -12,24 +18,40 @@ public class BoardManager : MonoBehaviour
 
     void Start()
     {
+        Initialize();
+        GenerateBoard();
+    }
+
+    void Initialize()
+    {
+        m_BoardData = new CellData[Width, Height];
         m_Tilemap = GetComponentInChildren<Tilemap>();
 
+
+    }
+
+    void GenerateBoard()
+    {
         for (int y = 0; y < Height; ++y)
         {
             for (int x = 0; x < Width; ++x)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
 
                 if (x == 0 || y == 0
                     || x == Width - 1
                     || y == Height - 1)
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    m_BoardData[x, y].Passable = true;
                 }
+
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
